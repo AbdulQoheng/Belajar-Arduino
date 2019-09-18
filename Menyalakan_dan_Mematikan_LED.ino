4ebc7050134d7487c96c4e7e97030ebf694bc7c9
AbdulQoheng <54624522+AbdulQoheng@users.noreply.github.com>
2019-09-19 02:34:19 +0700

    Belajar_1

---
+++ Menyalakan_dan_Mematikan_LED.ino
@@ -0,0 +1,17 @@
+#define PinLed 13 // Deklarasi pin 13 (terdapat led pada pin 13) 
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(PinLed, OUTPUT); // Memberikan mode ouput pada pin 13 (PinLed)
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  digitalWrite(PinLed, HIGH); // Memberi nilai HIGH atau 1 pada pin 13
+  delay(1000); // Memberikan waktu dengan satuan milisecond
+  digitalWrite(PinLed, LOW); // Memberi nilai LOW atau 0 pada pin 13
+  delay(500); // Memberikan waktu
+
+}
